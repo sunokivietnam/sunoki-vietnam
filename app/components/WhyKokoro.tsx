5792c898811c87d5eb3334e5d3a7980db63655b4
sunokivietnam <sunoki.vietnam@gmail.com>
2026-07-09 14:20:10 +0700

    Update About section

--- app/components/WhyKokoro.tsx
+++ app/components/WhyKokoro.tsx
@@ -67,8 +67,7 @@ export default function WhyKokoro() {
 
         {/* Cards */}
 
-        <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
-
+        <div className="grid grid-cols-2 gap-5 lg:grid-cols-3">
           {features.map((feature) => {
 
             const Icon = feature.icon;
