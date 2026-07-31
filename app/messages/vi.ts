e4746c7ad90fd02d87c495a30fe339f1b79b2df7
sunokivietnam <sunoki.vietnam@gmail.com>
2026-07-31 13:51:42 +0700

    Add Model Plans section

--- app/messages/vi.ts
+++ app/messages/vi.ts
@@ -677,12 +677,12 @@ Nhật Bản từ năm 1963.`,
 
 description:
   `Với hơn 60 năm kinh nghiệm trong thi công và quản lý chất lượng, 
-  KOKORO Living mang đến những giải pháp không gian bền vững, 
-  tinh tế và đáng tin cậy tại Việt Nam..`,
+  KOKORO Living mang đến những giải pháp không gian bền vững, tinh tế và đáng tin cậy tại Việt Nam..`,
 
 descriptionMobile:
 `Với hơn 60 năm kinh nghiệm trong 
 thi công và quản lý chất lượng,
+
 KOKORO Living mang đến 
 những giải pháp không gian bền vững, 
 tinh tế và đáng tin cậy tại Việt Nam.`,
