5facfffe0ea5f4be78e6035e0bc21a79be0a0c13
sunokivietnam <sunoki.vietnam@gmail.com>
2026-08-01 14:49:52 +0700

    Add specifications section to model plans

--- app/components/ModelPlans.tsx
+++ app/components/ModelPlans.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Lightbox from "yet-another-react-lightbox";
 import "yet-another-react-lightbox/styles.css";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp, Check } from "lucide-react";
 
 import Container from "./ui/Container";
 import SectionTitle from "./ui/SectionTitle";
