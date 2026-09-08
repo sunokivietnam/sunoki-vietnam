20c93684a02de1c4a508079fb87bb2289fdc8b03
sunokivietnam <sunoki.vietnam@gmail.com>
2026-09-08 08:26:28 +0700

    Update site

--- app/messages/vi.ts
+++ app/messages/vi.ts
@@ -343,7 +343,7 @@ như ngoài đời thực.`,
     "Tỉ mỉ trong từng chi tiết, chuẩn mực trong từng công đoạn.\nChúng tôi ứng dụng tiêu chuẩn Nhật Bản vào thiết kế và thi công,\nmang đến những không gian bền vững, tiện nghi và đáng tin cậy.",
 
   descriptionMobile:
-    "Với sự tỉ mỉ trong từng chi tiết,\nthiết kế và thi công theo tiêu chuẩn Nhật Bản,\nchúng tôi tạo nên\nkhông gian sống đáng tin cậy.",
+    "Tỉ mỉ trong từng chi tiết,\nchuẩn mực trong từng công đoạn.\nChúng tôi ứng dụng tiêu chuẩn Nhật Bản vào thiết kế và thi công,\nmang đến những không gian bền vững, tiện nghi và đáng tin cậy.",
 
   features: [
     {
